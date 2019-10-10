8da3925a22181f6a761cf32d0bb56f76f10e7265
Dewayne Richardson <dewayne.richardson@valimail.com>
2019-10-10 13:55:48 -0600

    minor cleanup

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 pushd `dirname $0` > /dev/null
 SCRIPT_DIR=`pwd -P`
 popd > /dev/null
-BUILD_DIR=${SCRIPT_DIR}/output
+BUILD_DIR=${SCRIPT_DIR}/docs
 
 rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
@@ -14,9 +14,9 @@ docker pull asciidoctor/docker-asciidoctor
 
 ./check_rule_ids.sh
 
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor -D /documents/output index.adoc
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -D /documents/output index.adoc
-docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 -D /documents/output index.adoc
+docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor -D /documents/docs index.adoc
+docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -D /documents/docs index.adoc
+docker run -v ${SCRIPT_DIR}:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 -D /documents/docs index.adoc
 
 cp -r assets ${BUILD_DIR}/
 cp -r -n legacy/* ${BUILD_DIR}/
