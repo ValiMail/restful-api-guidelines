699c22fbacf26335765ececd0e852b86e204c5c4
Dewayne Richardson <dewayne.richardson@valimail.com>
2019-10-16 14:46:11 -0600

    updated for testing

--- rebuild-releases.sh
+++ rebuild-releases.sh
@@ -6,22 +6,11 @@ for t in $tags
 do
   echo "t: $t"
   git checkout tags/$t
+  ./build.sh
+  latest_tag=$(git describe --abbrev=0 --tags)
+  cp -r docs releases/$latest_tag
+  rm releases/current
+  cd releases
+  ln -s $latest_tag current
+  git checkout master
 done;
-exit 0
-echo "tag_count: $tag_count"
-
-release_count=$(find releases -mindepth 1 -maxdepth 1 -type d | wc -l)
-echo "release_count: $release_count"
-
-if [ "$tag_count" -gt "$release_count" ] 
-  then
-	echo "releasing...."
-    ./build.sh
-    latest_tag=$(git describe --abbrev=0 --tags)
-	cp -r docs releases/$latest_tag
-	rm releases/current
-	cd releases
-	ln -s $latest_tag current
-else
-	echo "The number of git tags matches the number of documentation releases"
-fi
