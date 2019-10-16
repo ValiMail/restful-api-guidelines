bdcc24d3e717115524f98036f8724ffc856b0ba2
Dewayne Richardson <dewayne.richardson@valimail.com>
2019-10-16 14:55:59 -0600

    updated for testing

--- rebuild-releases.sh
+++ rebuild-releases.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-
+mkdir -p releases
 tags=$(git tag)
 for t in $tags
 do
   echo "t: $t"
   git checkout tags/$t
   ./build.sh
+  # clean the build output
+  rm -rf output
   latest_tag=$(git describe --abbrev=0 --tags)
   cp -r output releases/$latest_tag
   rm releases/current
