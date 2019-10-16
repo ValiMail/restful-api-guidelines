0a282673267adda7b7ae9e9aa0ef6e485af5b71a
Dewayne Richardson <dewayne.richardson@valimail.com>
2019-10-16 15:03:31 -0600

    fixed taging bug

--- rebuild-releases.sh
+++ rebuild-releases.sh
@@ -4,14 +4,13 @@ mkdir -p releases
 tags=$(git tag)
 for t in $tags
 do
-  echo "t: $t"
+  echo "Rebuilding Documentation based on Tag: $t"
   git checkout tags/$t
   ./build.sh
-  latest_tag=$(git describe --abbrev=0 --tags)
-  cp -r output releases/$latest_tag
+  cp -r output releases/$t
   # shell out to create the symbolic link
-  (cd releases && rm current && ln -s $latest_tag current)
+  (cd releases && rm current && ln -s $t current)
+  git checkout master
   # clean the build output
   rm -rf output
-  git checkout master
 done;
