74f1d6bf654737e4f4b7313fe12a1b8f94a35e30
Dewayne Richardson <dewayne.richardson@valimail.com>
2019-10-16 14:57:00 -0600

    fixed rebuild bug

--- rebuild-releases.sh
+++ rebuild-releases.sh
@@ -7,12 +7,12 @@ do
   echo "t: $t"
   git checkout tags/$t
   ./build.sh
-  # clean the build output
-  rm -rf output
   latest_tag=$(git describe --abbrev=0 --tags)
   cp -r output releases/$latest_tag
   rm releases/current
   cd releases
   ln -s $latest_tag current
+  # clean the build output
+  rm -rf output
   git checkout master
 done;
